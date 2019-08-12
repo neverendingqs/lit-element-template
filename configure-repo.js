06f5f6ddce8615dc46b9fa1451672103ca6da2ba
Margaree Peacocke <margaree.p@gmail.com>
2019-08-12 11:03:02 -0400

    use synchrnous functions

--- configure-repo.js
+++ configure-repo.js
@@ -9,44 +9,32 @@ const config = {
 };
 
 function updateFiles(path) {
-	return new Promise((resolve) => {
-		if (fs.existsSync(path)) {
-			const files = fs.readdirSync(path);
-			files.forEach((file) => {
-				const currentPath = `${path}/${file}`;
-				if (fs.lstatSync(currentPath).isDirectory()) {
-					resolve(updateFiles(currentPath));
-				} else {
-					resolve(replaceTextWithConfigs(currentPath));
-				}
-			});
-		}
-	});
+	if (fs.existsSync(path)) {
+		const files = fs.readdirSync(path);
+		files.forEach((file) => {
+			const currentPath = `${path}/${file}`;
+			if (fs.lstatSync(currentPath).isDirectory()) {
+				updateFiles(currentPath);
+			} else {
+				replaceTextWithConfigs(currentPath);
+			}
+		});
+	}
 }
 
 function replaceTextWithConfigs(fileName) {
-	return new Promise((resolve) => {
-		if (fileName.indexOf('configure-repo.js') !== -1
-			|| fileName.indexOf('.git') !== -1
-			|| fileName.indexOf('node_modules') !== -1) {
-			return;
-		}
-		fs.readFile(fileName, 'utf8', (err, data) => {
-			if (err) {
-				return console.log(err);
-			}
-			const result = data.replace(/<%= name %>/g, config.name)
-				.replace(/<%= shortName %>/g, config.shortName)
-				.replace(/<%= packageName %>/g, config.packageName)
-				.replace(/<%= description %>/g, config.description)
-				.replace(/<%= codeowner %>/g, config.codeowner);
-
-			fs.writeFile(fileName, result, 'utf8', (err) => {
-				if (err) return console.log(err);
-				resolve();
-			});
-		});
-	});
+	if (fileName.indexOf('configure-repo.js') !== -1
+		|| fileName.indexOf('.git') !== -1
+		|| fileName.indexOf('node_modules') !== -1) {
+		return;
+	}
+	const data = fs.readFileSync(fileName, 'utf8');
+	const result = data.replace(/<%= name %>/g, config.name)
+		.replace(/<%= shortName %>/g, config.shortName)
+		.replace(/<%= packageName %>/g, config.packageName)
+		.replace(/<%= description %>/g, config.description)
+		.replace(/<%= codeowner %>/g, config.codeowner);
+	fs.writeFileSync(fileName, result, 'utf8');
 }
 
 function moveFile(source, destination) {
@@ -60,28 +48,21 @@ function moveFile(source, destination) {
 }
 
 const path = './';
+
 console.log(`Filling in config values for ${config.name}...`);
-return updateFiles(path).then(() => {
-	const year = new Date().getFullYear().toString();
-	fs.readFile('LICENSE', 'utf8', (err, data) => {
-		if (err) {
-			return console.log(err);
-		}
-		const result = data.replace(/<%= year %>/g, year);
+updateFiles(path);
+const year = new Date().getFullYear().toString();
+const licenseData = fs.readFileSync('LICENSE', 'utf8');
+const licenseResult = licenseData.replace(/<%= year %>/g, year);
+fs.writeFileSync('LICENSE', licenseResult, 'utf8');
 
-		fs.writeFile('LICENSE', result, 'utf8', (err) => {
-			if (err) return console.log(err);
-		});
-	});
+console.log('Moving files...');
+moveFile('_element.js', `${config.shortName}.js`);
+moveFile('test/_element.html', `test/${config.shortName}.html`);
+moveFile('travis.yml', '.travis.yml');
+moveFile('.CODEOWNERS', 'CODEOWNERS');
 
-	console.log('Moving files...');
-	moveFile('_element.js', `${config.shortName}.js`);
-	moveFile('test/_element.html', `test/${config.shortName}.html`);
-	moveFile('travis.yml', '.travis.yml');
-	moveFile('.CODEOWNERS', 'CODEOWNERS');
+fs.unlinkSync('README.md');
+moveFile('README_element.md', 'README.md');
 
-	fs.unlinkSync('README.md');
-	moveFile('README_element.md', 'README.md');
-
-	console.log(`Repo for ${config.name} successfully configured.`);
-});
+console.log(`Repo for ${config.name} successfully configured.`);
