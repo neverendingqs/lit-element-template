2a29300d0abc9276d095373e14b55343b2179501
Mark Tse <neverendingqs@users.noreply.github.com>
2019-11-06 14:57:19 -0500

    step 2.

--- first.js
+++ first.js
@@ -30,13 +30,25 @@ class First extends LitElement {
 		]
 	}
 
+	addTodo() {
+		const newTodo = this.shadowRoot.getElementById('new-todo').value;
+		if(newTodo) {
+			this.todos = [
+				...this.todos,
+				{ name: newTodo }
+			];
+		}
+	}
+
 	render() {
 		return html`
 			<h2>Hello ${this.prop1}!</h2>
+			<input type="text" id="new-todo" placeholder="New TODO">
+			<button type="button" @click="${this.addTodo}">Add TODO</button>
 			<ol>
-			${this.todos.map(({ name }, i) => html`
-				<li>${name}</li>
-			`)}
+				${this.todos.map(({ name }, i) => html`
+					<li>${name}</li>
+				`)}
 			</ol>
 		`;
 	}
